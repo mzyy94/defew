ad33265a4beeb24f632d4e80ed8cadb837883b19
Yuki MIZUNO <mizuyuu0904@gmail.com>
2025-03-05 22:45:04 +0900

    feat: create Dew macro

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, MetaList};
+
+#[proc_macro_derive(Defew, attributes(new))]
+pub fn defew(input: TokenStream) -> TokenStream {
+    let input = &parse_macro_input!(input as DeriveInput);
+    let DataStruct { fields, .. } = match &input.data {
+        Data::Struct(v) => v,
+        _ => panic!("Defew only supports structs"),
+    };
+
+    let mut default_values = Vec::new();
+    for field in fields {
+        if let Some(attr) = field.attrs.first() {
+            let ident = field.ident.as_ref().unwrap();
+            let MetaList { tokens, .. } = attr.meta.require_list().unwrap();
+
+            default_values.push(quote! {
+                #ident: #tokens,
+            });
+        }
+    }
+
+    let struct_name = &input.ident;
+    let (impl_generics, _, where_clause) = &input.generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics #struct_name #where_clause {
+            pub fn new() -> Self {
+                Self {
+                    #(#default_values)*
+                    ..Default::default()
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
