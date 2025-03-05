e50da1bc30840cefd0e2296a0456c17996f54e94
Yuki MIZUNO <mizuyuu0904@gmail.com>
2025-03-05 23:12:23 +0900

    feat: check delimiter format

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, MetaList};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, MacroDelimiter, MetaList};
 
 #[proc_macro_derive(Defew, attributes(new))]
 pub fn defew(input: TokenStream) -> TokenStream {
@@ -19,7 +19,16 @@ pub fn defew(input: TokenStream) -> TokenStream {
         }
         if let Some(attr) = field.attrs.first() {
             let ident = field.ident.as_ref().unwrap();
-            let MetaList { tokens, .. } = attr.meta.require_list().unwrap();
+            let MetaList {
+                tokens,
+                delimiter: MacroDelimiter::Paren(_),
+                ..
+            } = attr.meta.require_list().unwrap()
+            else {
+                return syn::Error::new_spanned(&attr, "Defew supports #[new(value)] syntax")
+                    .to_compile_error()
+                    .into();
+            };
 
             default_values.push(quote! {
                 #ident: #tokens,
